33be77c9bd697815454ca945f2bfb07e16e9a57c
Amit Singh Deora <125014702+amitsinghs98@users.noreply.github.com>
2025-09-06 10:48:38 -0700

    Create Footer.tsx

---
+++ components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="bg-gray-100 p-4 mt-8 text-center">
+      <p>&copy; 2025 JobPortal. All rights reserved.</p>
+    </footer>
+  )
+}
